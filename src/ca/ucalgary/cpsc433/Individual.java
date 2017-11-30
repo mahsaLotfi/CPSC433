cbe5c1be5e384e4e9e207ec78e542bcbfb3340fd
Michael <xiangyu.han@ucalgry.ca>
2017-11-30 17:04:15 -0600

    compare method and Individual constructor

--- src/ca/ucalgary/cpsc433/Individual.java
+++ src/ca/ucalgary/cpsc433/Individual.java
@@ -1,11 +1,34 @@
 package ca.ucalgary.cpsc433;
 
+import ca.ucalgary.cpsc433.schedule.Schedule;
+
 public class Individual implements Comparable<Individual>{
+	private Schedule schedule;
+	private int fitness;
+	
+	/**
+	 * 
+	 * @param arg0 complete schedule
+	 * @param arg1 the fitness value evaluated
+	 */
+	public Individual(Schedule arg0, int arg1) {
+		schedule = arg0;
+		fitness = arg1;
+	}
 
 	@Override
-	public int compareTo(Individual o) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int compareTo(Individual e) {
+		if (this.fitness < e.fitness) return -1;
+		else if (this.fitness == e.fitness) return 0;
+		else return 1;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public Schedule getSchedule () {
+		return schedule;
 	}
 
 }
