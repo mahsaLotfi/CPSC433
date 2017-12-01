fdd7d8f21429f8c7adc09644ba72afd3907937eb
Obicere <mattwzawislak@gmail.com>
2017-11-30 19:04:09 -0700

    Added example print out of parsing in main. Make sure to run with a a program argument.

--- src/ca/ucalgary/cpsc433/Main.java
+++ src/ca/ucalgary/cpsc433/Main.java
@@ -1,12 +1,31 @@
 package ca.ucalgary.cpsc433;
 
-import java.util.Random;
+import ca.ucalgary.cpsc433.environment.Environment;
+import ca.ucalgary.cpsc433.environment.Lab;
+import ca.ucalgary.cpsc433.environment.Lecture;
+import ca.ucalgary.cpsc433.io.InputParser;
 
 /**
  * @author Obicere
  */
 public class Main {
-	public static void main(String[] args) {
-	Random rn = new Random();
-	}
+    public static void main(final String[] args) {
+        final Environment environment;
+        try {
+            environment = InputParser.parseEnvironment(args[0]);
+        } catch (final Exception e) {
+            e.printStackTrace();
+            return;
+        }
+        for (final Lecture lecture : environment.getLectures()) {
+            System.out.println(lecture);
+            if (lecture.getLabs() != null) {
+                for (final Lab lab : lecture.getLabs()) {
+                    System.out.print('\t');
+                    System.out.println(lab);
+                }
+            }
+            System.out.println();
+        }
+    }
 }
