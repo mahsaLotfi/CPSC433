4e9ae332690aba69650515a4c088816b7c42064e
mahsaLotfi <32718597+mahsaLotfi@users.noreply.github.com>
2017-12-07 23:28:48 -0700

    Documentation

--- src/ca/ucalgary/cpsc433/environment/Pair.java
+++ src/ca/ucalgary/cpsc433/environment/Pair.java
@@ -1,6 +1,11 @@
 package ca.ucalgary.cpsc433.environment;
 
 /**
+ * This is a class for Pair courses. One of the general soft constraints are
+ * courses that are pair together, which department knows that students will
+ * not get these courses together. So it's ok to assign these courses at the
+ * same time.  
+ * 
  * @author Obicere
  */
 public class Pair {
@@ -9,6 +14,11 @@ public class Pair {
 
     private final Course right;
 
+    /**
+     * Constructor to initialize each pair courses.
+     * @param left
+     * @param right
+     */
     public Pair(final Course left, final Course right) {
         if (left == null) {
             throw new NullPointerException("left must be non-null.");
@@ -20,6 +30,11 @@ public class Pair {
         this.right = right;
     }
 
+    /**
+     * Getters
+     * @return Course
+     */
+    
     public Course getLeft() {
         return left;
     }
@@ -28,11 +43,20 @@ public class Pair {
         return right;
     }
 
+    /**
+     * Returns hash code value of left course + right course
+     * @return int
+     */
     @Override
     public int hashCode() {
         return left.hashCode() * 31 + right.hashCode();
     }
 
+    /**
+     * Returns true if the argument's left & right courses are 
+     * equal to the current object. False if not.
+     * @return boolean
+     */
     @Override
     public boolean equals(final Object o) {
         if (this == o) {
@@ -48,6 +72,9 @@ public class Pair {
         return getLeft().equals(other.getLeft()) && getRight().equals(other.getRight());
     }
 
+    /**
+     * @return String
+     */
     @Override
     public String toString() {
         return "Pair[" + left + ", " + right + "]";
